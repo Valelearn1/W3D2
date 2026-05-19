30371d48ef4a6d6056f86f2706b7d20739f0e338
Valelearn1 <valentina-ferro@hotmail.it>
2026-05-19 16:56:45 +0200

    end exercise

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,23 +11,24 @@ const contatore = document.querySelector("#contatore");
 function aggiungiTask(testo, priorita, eliminaTesto = "Elimina") {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  const id = Date.now();
+  const id = new Date().toLocaleString();
   const data = document.createElement("span");
   const elimina = document.createElement("button");
 
   li.textContent = testo;
   span.textContent = priorita;
-  data.textContent = new Date().toLocaleString();
+  data.textContent = id;
   elimina.textContent = eliminaTesto;
 
+  li.setAttribute("data-id", id); //settare l'attributo data-id
   li.classList.add(`priorita-${priorita}`);
   span.classList.add("badge");
   data.classList.add("data");
   elimina.classList.add("bottone-aggiungi");
 
   li.appendChild(span);
-  li.appendChild(data);
   li.appendChild(elimina);
+  li.appendChild(data);
   lista.appendChild(li);
 
   aggiornaContatore();
@@ -38,11 +39,25 @@ function aggiornaContatore() {
   contatore.textContent = tasks.length;
 }
 
+const evidenzia = (indice) => {
+  const tasks = lista.querySelectorAll("li");
+  const task = tasks[indice];
+  task.classList.add("evidenziato");
+};
+
+const togliEvidenza = (indice) => {
+  const tasks = lista.querySelectorAll("li");
+  const task = tasks[indice];
+  task.classList.remove("evidenziato");
+};
+
 aggiungiTask("Sistemare casa", "media");
 aggiungiTask("Studiare JavaScript", "alta");
 aggiungiTask("Farsi un caffè", "bassa");
 aggiungiTask("Prenotare visita medica", "alta");
 aggiungiTask("Riposarsi", "bassa");
+
+evidenzia(1);
 
 /* SCRIVI QUI LE TUE FUNZIONI:
    1. Modifica aggiungiTask per accettare priorita
